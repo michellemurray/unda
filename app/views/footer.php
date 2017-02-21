abf9c4d682c0ffc8952447b20ddfe9f8547a8f05
Lachlan Enderlin <lendo1995@gmail.com>
2017-02-21 18:43:45 +1000

    Created footer

--- app/views/footer.php
+++ app/views/footer.php
@@ -1,5 +1,17 @@
         </main>
         <footer>
-        <footer>
+            <div id="footerLinks">
+                <div class="footer-link-wrapper">
+                    <a href='' class="footer-link">
+                        <i class="fa fa-facebook" aria-hidden="true"></i>
+                    </a>
+                </div>
+                <div class="footer-link-wrapper">
+                    <a href='' class="footer-link">
+                        <i class="fa fa-twitter" aria-hidden="true"></i>
+                    </a>
+                </div>
+            </div>
+        </footer>
     </body>
 </html>
