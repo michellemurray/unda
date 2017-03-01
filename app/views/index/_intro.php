4ad614df5355c075eb06f6990e04241ec0720385
Lachlan Enderlin <lendo1995@gmail.com>
2017-03-01 12:43:28 +1000

    .

--- app/views/index/_intro.php
+++ app/views/index/_intro.php
@@ -1,9 +1,13 @@
 <section>
     <h2 class="section-title">What is Unda?</h2>
-    <p>Unda is an online video sharing site that maintains focus on gaining exposure for videos.</p>
     <p>
-        Based in Brisbane, Australia, Unda started with the mission to help small channels to achieve
-        thier goal to achieve notority for their content without having to hire a publisher, pay
+        Unda is an online video sharing site that maintains focus on gaining exposure for videos. Every
+        day in the comments section of popular video streaming sites, small channels will advertise themselves
+        in attempt to expand their audience. Unda removes that.
+    </p>
+    <p>
+        Based in Brisbane, Australia, Unda started with the mission to help content creators to achieve
+        achieve notority for their content without having to hire a publisher, pay
         for expsensive marketing, or spending hours every week on social media sites like Facebook
         and Reddit.
     </p>
--- app/views/index/_more.php
+++ app/views/index/_more.php
@@ -1,20 +1,38 @@
 <section>
-    <h2 class="section-title">How can Unda help me?</h2>
+    <h2 class="section-title">So how does Unda help small channels?</h2>
     <p>
         Unlike most video sharing sites, Unda aims to garner as many viewers as possible for content
-        creators without the need of third-party social media sites. A newly published video will
-        recieve a minimum of 5,000 views within the first 24 hours, with the goal of boosting your
-        channel.
+        creators without the need of third-party social media sites. A newly published video will be
+        placed into a newsfeed for at most 24 hours where the Unda algorithm splits the content fairly
+        to avoid saturation and gain exposure for all videos.
     </p>
+    <div class='wrap'>
+        <div class="process">
+            <h3 class="number">
+                <span>1</span>
+            </h3>
+            Upload & publish new video
+        </div>
+        <div class="process">
+            <h3 class="number">
+                <span>2</span>
+            </h3>
+            Video placed in news feed for 24 hours or until it hits the 5000 view cap
+        </div>
+        <div class="process">
+            <h3 class="number">
+                <span>3</span>
+            </h3>
+            View cap increases by 5 views for every like it recieves
+        </div>
+    </div>
+    <br>
+    <br>
     <p>
-        Unda focuses on sharing newly uploaded videos rather than soley supporting a popularity
-        contest because we believe everyone deserves a chance. Upon upload your video will be placed through a
-        newsfeed with all other videos for 24 hours. Your video will remain in this newsfeed for at most 24 hours,
-        or until it reaches 5000 views. The view cap is extended by 5 views for every like it recieves in the 24
-        hour time frame.
+        Once the video is removed from the news feed it is simply hosted like any other video
+        and can be viewed at any time as long as it is public. New videos can also trend but
+        are held to different standards due to the boost they recieve.
     </p>
-    <p>
-        Simply put if your video recieves no likes the max number of views in the newsfeed will be 5000, but 6000 views if
-        it recieves 200 likes. Once your video reaches its view or time limit, it will be removed from the feed for other videos.
-    </p>
+    <br>
+    <br>
 </section>
