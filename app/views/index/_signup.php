be70129d381af182bf45489ddf23fffa8c29ada3
Lachlan Enderlin <lendo1995@gmail.com>
2017-03-04 21:07:34 +1000

    .

--- app/views/index/_signup.php
+++ app/views/index/_signup.php
@@ -3,16 +3,20 @@
         <h2>
             Interested?
         </h2>
-        <h4>Join our mailing list. We won't send you spam.</h4>
+        <h4>Join our mailing list for future updates.</h4>
     </div>
     <form id="signUpForm" action='<?= URL . 'submit'; ?>' method='post' class="inline-block">
-        <div class='form-group inline-block'>
+        <div class='form-group block'>
             <label>Name</label>
             <input type='text' name='name' required/>
         </div>
         <div class='form-group block'>
             <label>Email</label>
             <input type='email' name='email' required />
+        </div>
+        <div class='form-group block'>
+            <input type='checkbox' name='beta' checked/>
+            <label class='inline-block'>I want to be considered for beta testing</label>
         </div>
         <div class='form-group block'>
             <button>Submit</button>
