d46582bf719bc179cf35b3c8be20a659085c0eb8
michellemurray <mmurray@redhat.com>
2017-04-24 16:31:19 +1000

    Patch - typo fixes

--- app/views/index/_intro.php
+++ app/views/index/_intro.php
@@ -3,12 +3,12 @@
     <p>
         Unda is an online video sharing site that maintains focus on gaining exposure for videos. Every
         day in the comments section of popular video streaming sites, small channels will advertise themselves
-        in attempt to expand their audience. Unda removes that.
+        in an attempt to expand their audience. Unda removes that.
     </p>
     <p>
-        Based in Brisbane, Australia, Unda started with the mission to help content creators to achieve
+        Based in Brisbane, Australia, Unda started with the mission to help content creators to
         achieve notority for their content without having to hire a publisher, pay
-        for expsensive marketing, or spending hours every week on social media sites like Facebook
+        for expensive marketing, or spending hours every week on social media sites like Facebook
         and Reddit.
     </p>
 </section>
